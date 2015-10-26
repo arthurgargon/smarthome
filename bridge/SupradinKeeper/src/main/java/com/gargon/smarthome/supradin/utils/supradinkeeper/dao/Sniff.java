7333fe2a0ec8c9e378da8d85963cd42c6d3d35ce
arthurgargon <arthurgargon@gmail.com>
2015-10-26 16:25:35 +0400

    revert

---
+++ bridge/SupradinKeeper/src/main/java/com/gargon/smarthome/supradin/utils/supradinkeeper/dao/Sniff.java
@@ -0,0 +1,15 @@
+package com.gargon.smarthome.supradin.utils.supradinkeeper.dao;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+
+/**
+ *
+ * @author gargon
+ */
+@Entity
+@Table(name="hb_user")
+public class Sniff extends DAO{
+    
+}
