2df5c979e8f75cd34f6a898f79145e4f0c04bb50
arthurgargon <arthurgargon@gmail.com>
2016-01-10 00:45:40 +0400

    android command changed

--- bridge/SupradinConnector/src/com/gargon/smarthome/commands/Commands.java
+++ bridge/SupradinConnector/src/com/gargon/smarthome/commands/Commands.java
@@ -603,37 +603,21 @@ public class Commands {
     }
 
 
-    /**
-     * Проверяет должен ли быть планшет в ванной заблокирован или разблокирован
-     * по входящему сообщению и текущему состоянию
-     *
-     * @param message входящее сообщение для анализа
-     * @return возвращает true - если планшет должен быть разблоокирован; false
-     * - если планшет должен быть разблокирован; null - если переданное
-     * сообщение не имеет значения
-     */
-    public static Boolean isPadInBathtroomShouldBeUnlocked(SupradinDataMessage message, boolean isNowUnlocked) {
-        if (message != null) {
-            switch (message.getCommand()) {
-                case Clunet.COMMAND_ANDROID:
-                    if (message.getData().length == 1){
-                        switch(message.getData()[0]){
-                            case 0:
-                                return false;
-                            case 1: 
-                                return true;
-                            case 2:
-                                return !isNowUnlocked;
-                        }
-                    }
+      public static Integer checkAndroidInBathtroomCommand(SupradinDataMessage message) {
+        if(message != null) {
+            switch(message.getCommand()) {
+            case Clunet.COMMAND_LIGHT_LEVEL_INFO:
+                if(message.getSrc() == Clunet.ADDRESS_BATH_SENSORS && message.getData().length == 2) {
+                    return message.getData()[0] == 1 ? 1 : 0;
+                }
                 break;
-                case Clunet.COMMAND_LIGHT_LEVEL_INFO:
-                    if (message.getSrc() == Clunet.ADDRESS_BATH_SENSORS
-                            && message.getData().length == 2) {
-                        return message.getData()[0] == 0x01;
-                    }
+            case Clunet.COMMAND_ANDROID:
+                if(message.getData().length == 1) {
+                    return (int) message.getData()[0];
+                }
             }
         }
+
         return null;
     }
 
