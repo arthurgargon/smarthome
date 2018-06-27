170d022d31bd7ae155ca718822dd537f0e4eea11
arthurgargon <arthurgargon@gmail.com>
2018-06-28 01:41:42 +0400

    audiobox_v2

---
+++ devices/audiobox_v2/src/Audiobox/ClunetMulticastConfig.h
@@ -0,0 +1,11 @@
+#ifndef ClunetMulticastConfig_h
+#define ClunetMulticastConfig_h
+
+/* Device address (128-254) */
+#define CLUNET_DEVICE_ID 0x84
+
+/* Device name */
+#define CLUNET_DEVICE_NAME "Audiobox"
+
+
+#endif
