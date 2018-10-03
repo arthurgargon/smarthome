e2df09b4ff83b04c6aae41e968b4b6ec195910c7
arthurgargon <arthurgargon@gmail.com>
2018-10-04 00:37:28 +0400

    null checking

--- bridge/SupradinConnector/src/com/gargon/smarthome/utils/DataFormat.java
+++ bridge/SupradinConnector/src/com/gargon/smarthome/utils/DataFormat.java
@@ -7,19 +7,25 @@ package com.gargon.smarthome.utils;
 public class DataFormat {
 
     private static String byteArrayToHex(byte[] a) {
-        StringBuilder sb = new StringBuilder(a.length * 2);
-        for (byte b : a) {
-            sb.append(String.format("%02x", b & 0xff));
+        if (a != null) {
+            StringBuilder sb = new StringBuilder(a.length * 2);
+            for (byte b : a) {
+                sb.append(String.format("%02x", b & 0xff));
+            }
+            return sb.toString().toUpperCase();
         }
-        return sb.toString().toUpperCase();
+        return null;
     }
 
     private static byte[] hexStringToByteArray(String s) {
-        int len = s.length();
-        byte[] data = new byte[len / 2];
-        for (int i = 0; i < len; i += 2) {
-            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
-                    + Character.digit(s.charAt(i + 1), 16));
+        byte[] data = null;
+        if (s != null) {
+            int len = s.length();
+            data = new byte[len / 2];
+            for (int i = 0; i < len; i += 2) {
+                data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+                        + Character.digit(s.charAt(i + 1), 16));
+            }
         }
         return data;
     }
