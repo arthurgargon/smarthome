210772ba06e7e820eb88b6f98ae122f16ff65006
Arthur <arthurgargon@gmail.com>
2019-01-13 16:12:34 +0400

    project reorganize & move to maven

---
+++ bridge/MulticastConnector/src/com/gargon/smarthome/utils/DataFormat.java
@@ -0,0 +1,39 @@
+package com.gargon.smarthome.utils;
+
+/**
+ *
+ * @author gargon
+ */
+public class DataFormat {
+
+    private static String byteArrayToHex(byte[] a) {
+        StringBuilder sb = new StringBuilder(a.length * 2);
+        for (byte b : a) {
+            sb.append(String.format("%02x", b & 0xff));
+        }
+        return sb.toString().toUpperCase();
+    }
+
+    private static byte[] hexStringToByteArray(String s) {
+        int len = s.length();
+        byte[] data = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+                    + Character.digit(s.charAt(i + 1), 16));
+        }
+        return data;
+    }
+
+    public static String byteToHex(int value) {
+        return byteArrayToHex(new byte[]{(byte) value});
+    }
+
+    public static String bytesToHex(byte[] value) {
+        return byteArrayToHex(value);
+    }
+
+    public static byte[] hexToByteArray(String hex) {
+        return hexStringToByteArray(hex);
+    }
+
+}
