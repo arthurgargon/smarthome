21c89400f74f2fba35687fca7c9f6c24cfe9879f
Arthur <arthurgargon@gmail.com>
2019-01-17 22:45:54 +0400

    set disk

--- devices/esp-nixie/src/upload.bat
+++ devices/esp-nixie/src/upload.bat
@@ -1,4 +1,5 @@
-cd "C:\Users\gargon\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\2.5.0-beta2\tools"
-python espota.py -i 192.168.1.130 -p 8266 -f C:\Users\gargon\AppData\Local\Temp\arduino_build_911997/EspNixie.ino.bin
+C:
+cd "C:\Users\gargon\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\2.4.2\tools\"
+python espota.py -i 192.168.1.130 -p 8266 -f C:\Users\gargon\AppData\Local\Temp\arduino_build_525044/EspNixie.ino.bin
 
 pause
