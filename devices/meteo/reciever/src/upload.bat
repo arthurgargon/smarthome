b4e01720ad487fcc915291f84bb49fa6c4308866
artur <artur@infotrans-logistic.ru>
2019-01-31 17:58:22 +0400

    upload & spiffs 2.4.2

--- devices/meteo/reciever/src/upload.bat
+++ devices/meteo/reciever/src/upload.bat
@@ -1,4 +1,5 @@
-cd "C:\Users\gargon\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\2.4.1\tools"
-python espota.py -i 192.168.1.121 -p 8266 -f C:\Users\gargon\AppData\Local\Temp\arduino_build_365228/vw.ino.bin
+C:
+cd "C:\Users\gargon\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\2.4.2\tools"
+python espota.py -i 192.168.1.121 -p 8266 -f C:\Users\gargon\AppData\Local\Temp\arduino_build_120087/vw.ino.bin
 
 pause
