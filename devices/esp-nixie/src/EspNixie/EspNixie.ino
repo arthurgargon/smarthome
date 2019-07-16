5dcbdcede583dae27818b0eff05bccee8d52df73
gargon <arthurgargon@gmail.com>
2019-07-17 00:20:05 +0400

    esp8266 arduino 2.5.2 core
    *remove FSBrowserNG
    *use 2.5.2 esp8266 arduino core
    *use firmware ntp
    *reverse digits&leds order
    *fix /number REST command

--- devices/esp-nixie/src/EspNixie/EspNixie.ino
+++ devices/esp-nixie/src/EspNixie/EspNixie.ino
@@ -1,51 +1,57 @@
 /**
-    Use 2.4.2 esp8266 core
-    lwip 2 Higher bandwidth; CPU 160 MHz
-    128K SPIFFS
-  
-    dependencies:
-      https://github.com/PaulStoffregen/Time
-      https://github.com/gmag11/NtpClient (origin/develop)
-      https://github.com/gmag11/FSBrowserNG
-
-      https://github.com/me-no-dev/ESPAsyncUDP
-      https://github.com/me-no-dev/ESPAsyncWebServer
+ * Use 2.5.2 esp8266 core
+ * lwip 1.4 Higher bandwidth; CPU 160 MHz
+ * 1M (128K)
+ * 
+ * на lwip2 возникает ошибка, и не работает интернет(DNS?)
  * 
  */
 
-#include <FS.h>
-#include <FSWebServerLib.h>
-#include <ESPAsyncWebServer.h>
+#include <ESP8266WiFi.h>
+#include <ArduinoOTA.h>
 
+#include "ESPAsyncTCP.h"
+#include "ESPAsyncWebServer.h"
+ 
+#include <time.h>
 #include <TimeLib.h>
 
+#include "Credentials.h"
+#include "Tasks.h"
 #include "Narodmon.h"
 #include "Nixie.h"
 #include "Leds.h"
+
 #include "InsideTermometer.h"
-#include "Tasks.h"
+
 
 #define ALARM_DURATION 15000
 #define NUMBER_DURATION 5000
+
+IPAddress ip(192, 168, 1, 130); //Node static IP
+IPAddress gateway(192, 168, 1, 1);
+IPAddress subnet(255, 255, 255, 0);
 
 TaskWrapper* tw = new TaskWrapper();
 Narodmon* nm = new Narodmon(WiFi.macAddress());
 Leds* leds = new Leds();
 
-AsyncWebServer server(8080);
+AsyncWebServer server(80);
+
+
 
 
 //хранит параметр, переданный через метод /number
 //его передача в callContinuousTask возможна только
 //через глобальную переменную
-uint32_t number_to_show;
+char number_to_show[DIGITS_COUNT];
 
 
-/*void config_time(float timezone_hours_offset, int daylightOffset_sec,
+void config_time(float timezone_hours_offset, int daylightOffset_sec,
                  const char* server1, const char* server2, const char* server3) {
   configTime((int)(timezone_hours_offset * 3600), daylightOffset_sec, server1, server2, server3);
   INFO("Timezone: %f; daylightOffset: %d", timezone_hours_offset, daylightOffset_sec);
-}*/
+}
 
 void config_narodmon(String apiKey, uint8_t use_latlng, double lat, double lng, uint8_t radius) {
   nm->setApiKey(apiKey);
@@ -72,33 +78,40 @@ void show_value(float v, uint8_t pos_1, int num_frac){
  
         int lb = pos_1;
         float t = abs(v);
-        while (((int)(t = t/10)) > 0){
-          lb--;
-        }
+        do{
+          lb++;
+        }while (((int)(t = t/10)) > 0);
     
-        lb = max(lb, 0);
-        int rb = min(pos_1 + num_frac, leds_num - 1);
-        for (int i=lb; i<=rb; i++){
+        lb = _min(lb, leds_num);
+        int rb = _max(pos_1 - num_frac, 0);
+        for (int i=rb; i<lb; i++){
           leds[i] = MINUS_COLOR;
         }
     });
   }else{
     leds->backlight();
   }
-
 }
 
 uint8_t available_clock() {
-  return NTP.getLastNTPSync();
+  //если хоть раз синхронизировались, то отлично от 0
+  time_t t;
+  time(&t);
+  return t;
 }
 
 void _clock(){
-    time_t t = now();
+  time_t t;
+  time(&t);
+  if (t){
     int h = hour(t);
     int m = minute(t);
     int s = second(t);
     char p = t % 2;
     nixie_set(digit_code(h / 10, 1, 0), digit_code(h % 10, 1, p), digit_code(m / 10, 1, 0), digit_code(m % 10, 1, p), digit_code(s / 10, 1, 0), digit_code(s % 10, 1, p));
+  }else{
+    nixie_clear();
+  }
 }
 
 void show_clock() {
@@ -106,28 +119,12 @@ void show_clock() {
   leds->backlight();
 }
 
-/*
-uint8_t available_clock_adjust() {
-  return !available_clock();
-}
-
-void _clock_adjust(){
-    time_t t = now();
-    char p = (t / 500) % 6 ;    
-    nixie_set(digit_code(0, p==0, 1), digit_code(0, p==1, 1), digit_code(0, p==2, 1), digit_code(0, p==3, 1), digit_code(0, p==4, 1), digit_code(0, p==5, 1));
-}
-
-void show_clock_adjust() {
-  _clock_adjust();
-  leds->backlight();
-}
-*/
 uint8_t available_t() {
   return nm->hasT();
 }
 
 void show_t() {
-  show_value(nm->getT(), 3, 1);
+  show_value(nm->getT(), 2, 1);
 }
 
 uint8_t available_p() {
@@ -135,7 +132,7 @@ uint8_t available_p() {
 }
 
 void show_p() {
-  show_value(nm->getP(), 3, 1);
+  show_value(nm->getP(), 2, 1);
 }
 
 uint8_t available_h() {
@@ -143,7 +140,7 @@ uint8_t available_h() {
 }
 
 void show_h() {
-  show_value(nm->getH(), 4, 0);
+  show_value(nm->getH(), 2, 0);
 }
 
 uint8_t available_t_inside() {
@@ -151,20 +148,13 @@ uint8_t available_t_inside() {
 }
 
 void show_t_inside() {
-  show_value(insideTermometerTemperature(), 2, 3);
-}
-
-void show_number() {
-  show_value(number_to_show, 5, 0);
+  show_value(insideTermometerTemperature(), 3, 1);
 }
 
 void config_modes(uint32_t clock_duration, 
   uint32_t t_duration, uint32_t p_duration, uint32_t h_duration,
   uint32_t t_inside_duration) {
   tw->reset();
-  
-  //tw->addContinuousTask(clock_duration, available_clock_adjust, show_clock_adjust);
-  
   tw->addContinuousTask(clock_duration, available_clock, show_clock);
   tw->addContinuousTask(t_duration, available_t, show_t);
   if (p_duration){
@@ -196,13 +186,23 @@ void setup() {
   DEBUG("Booting");
   nixie_init();
 
-  SPIFFS.begin();
-  ESPHTTPServer.begin(&SPIFFS);
-    
+  WiFi.mode(WIFI_STA);
+
+  WiFi.begin(AP_SSID, AP_PASSWORD);
+  WiFi.config(ip, gateway, subnet);
+
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    delay(1000);
+    ESP.restart();
+  }
+  
   INFO("IP address: %s", WiFi.localIP().toString().c_str());
-
+  
+  //затем обновляет каждые 20 сек
+  //sntp_set_update_delay(20000);
+  
   //insideTermometerInit();
-
+  
   //server.on("/", handleRoot);
 
   server.on("/heap", HTTP_GET, [](AsyncWebServerRequest * request) {
@@ -217,9 +217,13 @@ void setup() {
     request->send(200, "text/plain", Logging._error);
   });
 
-  server.on("/time", HTTP_GET, [](AsyncWebServerRequest * request) {
-    request->send(200, "text/plain", String(millis()));
+  /*server.on("/uptime", HTTP_GET, [](AsyncWebServerRequest * request) {
+    request->send(200, "text/plain", NTP.getUptimeString());
   });
+
+  server.on("/synctime", HTTP_GET, [](AsyncWebServerRequest * request) {
+    request->send(200, "text/plain", NTP.getTimeDateString (NTP.getLastNTPSync ()));
+  });*/
 
   server.on("/reboot", HTTP_GET, [](AsyncWebServerRequest * request) {
     ESP.restart();
@@ -276,25 +280,34 @@ void setup() {
     request->send(200);
   });
 
+  /*server.on("/time", HTTP_GET, [](AsyncWebServerRequest * request) {
+    request->send(200, "text/plain", sntp_get_real_time(sntp_get_current_timestamp()));
+  });*/
+
+  server.on("/tz", HTTP_GET, [](AsyncWebServerRequest * request) {
+    config_time(request->arg("tz").toInt(), 0, "pool.ntp.org", "time.nist.gov", NULL);
+    request->send(200);
+  });
+
   server.on("/number", HTTP_GET, [](AsyncWebServerRequest *request) {
     int r = 404;
-    if(request->hasArg("v")){ //отображает последние(младшие) 6 цифр переданного номера
+    if(request->hasArg("v")){ //отображает последние(младшие) DIGITS_COUNT(6) цифр переданного номера
       String v = request->arg("v");
-      int length = _min(v.length(), 6);
+      int length = _min(v.length(), DIGITS_COUNT);
       v = v.substring(_max(v.length() - length, 0));
-      
-      //check digits in arg value
+
       char all_digits = 1;
       for (byte i = 0; i < v.length(); i++) {
         if (!isDigit(v.charAt(i))) {
-              all_digits = 0;
-            }
+            all_digits = 0;
+            break;
+         }
       }
       
       if (all_digits) {
-        number_to_show = v.toInt();
+         nixie_int(v.toInt(), 0, length, number_to_show);
          tw->callContinuousTask(NUMBER_DURATION, NULL, []() {
-            show_number();
+            nixie_set(number_to_show);
          });
         r = 200;
       }
@@ -314,7 +327,14 @@ void setup() {
   server.begin();
 
 
+  ArduinoOTA.setHostname("esp-nixie");
   ArduinoOTA.onStart([]() {
+    //if (ArduinoOTA.getCommand() == U_FLASH)
+    //  type = "sketch";
+    //else // U_SPIFFS
+    //  type = "filesystem";
+    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+
     ota_progress = 0;
     tw->callContinuousTask([]() {
       leds->set([&](CRGB* leds, uint8_t num_leds, uint8_t* brightness){
@@ -328,7 +348,7 @@ void setup() {
             }
           }
         });
-      nixie_set(ota_progress, 5);
+      nixie_set(ota_progress, 0);
     });
   });
 
@@ -343,9 +363,9 @@ void setup() {
   });
 
 
+  ArduinoOTA.begin();
 
-
-  //config_time(4, 0, "pool.ntp.org", "time.nist.gov", NULL);
+  config_time(4, 0, "pool.ntp.org", "time.nist.gov", NULL);
   config_narodmon("9M5UhuQA2c8f8", 1, 53.2266, 50.1915, 5);
   config_modes(10000, 5000, 5000, 0, 0);
   
@@ -363,6 +383,6 @@ void loop() {
     tw->update();
   }
 
-  ESPHTTPServer.handle();
+  ArduinoOTA.handle();
   yield();
 }
